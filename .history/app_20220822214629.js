84a007743cde14ff938328ab3a2f94ef6c500f40
ellisvelandia <eyis619@gmail.com>
2022-08-22 21:47:51 -0500

    spiderman

---
+++ .history/app_20220822214629.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://online-movie-database.p.rapidapi.com/auto-complete",
+  params: { q: "spiderman" },
+  headers: {
+    "X-RapidAPI-Key": "f7c77fb172msh1ef00876340a814p154163jsn8055208d264a",
+    "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
